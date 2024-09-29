8636806b9e9c0538c8eb74962334b706b97c8d07
Dr. Clocktor <jonas@sakejo.dk>
2024-09-29 23:42:49 +0200

    Delete portfolio-opgaver directory

--- portfolio-opgaver/startup-name-gen.js
+++
@@ -1,9 +0,0 @@
-const firstPart = ["Tech", "Inno", "Smart", "Blue", "Next", "Bright", "Quantum", "Rapid", "Future", "Digital"];
-const secondPart = ["Vision", "Solutions", "Systems", "Works", "Dynamics", "Hub", "Network", "Innovations", "Labs", "Path"];
-
-function getStartupName () {
-   let randomNumber = Math.round(Math.random() * 10)
-    let ranStartupName = firstPart[randomNumber] + secondPart[randomNumber]
-    console.log(ranStartupName)
-}
-getStartupName()
